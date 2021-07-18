ef945a41bf668aee1f85b45a10f6bb4a9265cf2a
Logan <logan.surface@my.tccd.edu>
2021-07-18 17:16:46 -0500

    Streamlining of serial comm. and minor refactoring
    + Refactored establish_handshake function to serial_handshake()
    + Flags for the handshake are now configurable
    + Doxygen comments
    + Removal of extraneous input validation

--- include/Configuration.h
+++ include/Configuration.h
@@ -1,4 +1,3 @@
 const long BAUDRATE = 115200;   // 115200 bits per sec.
-const int MAX_MSG_LENGTH = 3;   // In Bytes (R, G, B)
-const int NUM_LEDS = 100;       // Total leds in strip
 const int DATA_PIN = 6;         // Pin data line is on
+const int NUM_LEDS = 100;       // (32x2)+(18x2)
--- src/AmbianceController.cpp
+++ src/AmbianceController.cpp
@@ -2,8 +2,8 @@
  *  AmbianceController.cpp
  *  @brief Firmware written for Arduino devices, to interface with the corresponding
  *  Ambiance desktop application. The device acts as a bias lighting system for
- *  media displayed on the desktop's screen by setting dynamic color zones on a
- *  LED strip.
+ *  media displayed on the desktop's screen by updating dynamic color zones on a
+ *  LED strip behind the display.
  * 
  * @author Logan Surface
  * @date July 17, 2021
@@ -14,22 +14,17 @@
 #include <Configuration.h>
 
 bool connected = false;
-static CRGB leds[NUM_LEDS];
-
-/*
-Handshake protocol
-------------------
-- Loop while the connected flag is false
-- Repeatedly print 'R' to serial, signaling controller is ready
-- If a callback of 'R' is recieved on the serial buffer from device:
-    -> Toggle the connection flag
-    -> Print 'B' to serial, signaling okay to begin
-*/
+CRGB leds[NUM_LEDS];
 
 /**
- * Connect to the desktop application with a series of serial flags.
+ * Connect to the Ambiance desktop application using a series of serial flags.
+ * 
+ * @param ready_flag The byte printed to serial indicating ready for connection.
+ * @param proceed_flag The byte printed to serial signaling a connection was established.
+ * 
+ * @returns A boolean state of true upon a successful connection.
  */
-void establish_handshake() {
+bool serial_handshake(byte ready_flag, byte proceed_flag) {
     while(!connected) {
         Serial.print('R');
         delay(500);
@@ -38,8 +33,8 @@ void establish_handshake() {
             byte byte_in = Serial.read();
 
             if(byte_in == 'R') {
-                connected = true;
                 Serial.print('B');
+                return true;
             }
         }
     }
@@ -47,48 +42,34 @@ void establish_handshake() {
 
 void setup() {
     Serial.begin(BAUDRATE);
-    establish_handshake();
+    Serial.setTimeout(10);
+
+    connected = serial_handshake('R', 'B');
 
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
     static unsigned int led_ind = 0;
-    static uint8_t buffer[MAX_MSG_LENGTH];
-    static unsigned int buffer_ind = 0;
-    static unsigned long last_packet_time = millis();
 
     if(connected) {
         while(Serial.available() > 0) {
-            // If data present on serial, grab a single byte of it
-            uint8_t byte_in = Serial.parseInt();
+            uint8_t r_in = Serial.parseInt();
+            uint8_t g_in = Serial.parseInt();
+            uint8_t b_in = Serial.parseInt();
 
-            // Debugging output
-            Serial.println("Byte: " + String(byte_in));
+            leds[led_ind++] = CRGB(r_in, g_in, b_in);
 
-            // Add it to the filtered byte buffer (exc. EOL and OoR)
-            // with \n representing the end of an RGB triple
-            if(buffer_ind < MAX_MSG_LENGTH)
-            {
-                buffer[buffer_ind] = byte_in;
-                buffer_ind = buffer_ind + 1;
-            }
-            else {
-                // Clear buffer and set color for the current led
-                buffer_ind = 0;
-                leds[led_ind] = CRGB(buffer[0], buffer[1], buffer[2]);
-                // Debugging output
-                Serial.println("LED " + String(led_ind) + ": " + buffer[0] + ", " + buffer[1] + ", " + buffer[2]);
-                led_ind = led_ind + 1;
-
-                // If we have received all our led data then update
-                // strip and set index to beginning of next frame
-                if(led_ind >= NUM_LEDS) {
-                    FastLED.show();
-                    led_ind = 0;
-                }
+            // When a complete frame has been recieved, refresh the
+            // strip and move the index to beginning of next frame.
+            if(led_ind >= NUM_LEDS) {
+                FastLED.show();
+                led_ind = 0;
             }
         }
+
+        // If the data stream halts, the device has been disconnected
+        connected = false;
     }
-    else establish_handshake();
+    else connected = serial_handshake('R', 'B');
 }
